708af64f065f9bb66bbd934126a8d15618812e87
debobratade-github <debobratade@gmail.com>
2022-10-29 00:03:22 +0530

    Assignment-1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import {useState} from 'react'
+
 function App() {
+
+  const[data, setData] = useState(null)
+  const[len, setLen] = useState(null)
+  const[print, setPrint] = useState(false)
+
+  function getData(val){
+    setData(val.target.value)
+    let text = val.target.value
+    
+    let count=0
+        let newText=text.split("")
+        for(let i in newText){
+          newText[i]=newText[i].trim()
+          if(newText[i].length>0)
+          count++
+        }
+
+    setLen("Length : " + count)
+    setPrint(false);
+  }
+  function clear(){
+    document.getElementById("my_field").value = '';
+    setPrint('');
+    setData('');
+    setLen('')
+    
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="Title">
+   <h3> Name : Debobrata Dey  [ fn-rad-39UDL ]</h3>
+   <h3>React Assignment-1</h3>
     </div>
+    <div className='parent'>
+    <div className="App">
+      <h1>Text Printer</h1>
+
+      <input type='text' id='my_field' onChange={getData} placeholder=' Enter your text'/> <br/> <br/>
+      <button className='btn1' onClick={()=>{setPrint(true)}}>Submit</button>
+      <button className='btn2' onClick={clear}>clear</button>
+      
+      
+      
+      {
+        print?
+        <h2>{data}</h2>
+        :null
+      }
+      {
+        print?
+        <h2>{len}</h2>
+        :null
+      }
+    </div>
+    </div>
+    </>
   );
 }
 
